66b36967cb068f0bf9d13643873e859c0e580327
Shana Hu <shanawho@gmail.com>
2015-03-22 18:15:30 -0700

    edited styling and some markup

--- js/home.js
+++ js/home.js
@@ -1,25 +1,44 @@
 $(document).ready(function(){
-	$nav = $('.nav');
-	$nav.data('size', 'big');
+  var $nav = $('nav');
+  $nav.data('size', 'big');
 
-	$(window).scroll(function(){
-		if($(window).scrollTop() > window.innerHeight-130){ // position of menu from the top 
-			$nav.addClass('fixedClass');
-			if ($nav.data('size') == 'big') {
-				$nav.data('size','small').stop().animate({fontSize: ".8em", height:'7%', paddingTop:"0em"}, 300);
-			}
-		} else{
-			$nav.removeClass('fixedClass');
-			if ($nav.data('size') == 'small') {
-				$nav.data('size','big').stop().animate({fontSize: "1em", height:'15%', paddingTop:"1.5em"}, 300);
-			}
-		}
-	});
 
-	$(".rotate").textrotator({
- 	 	animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
-  	separator: "|", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-  	speed: 3500 // How many milliseconds until the next word show.
-	});
+  /**
+   ** Create and append responses to 'Design is' so that
+   ** JS loading slowly won't result in a block of text showing up on the page
+   **/
+  var $rotate = $('.rotate');
+  $rotate.text("the perfect fusion of creativity and logic|a way of thinking, a pursuit, a process|interdisciplinary problem-solving|everywhere, everything|iterating on a problem to find the best solution|a way to enhance people's lives");
+
+
+  /**
+   ** animation: You can pick the way it animates when rotating through words. 
+   ** Options are dissolve (default), fade, flip, flipUp, flipCube, 
+   ** flipCubeUp and spin.
+   **
+   ** separator: If you don't want commas to be the separator, you can define 
+   ** a new separator (|, &, * etc.) by yourself using this field.
+   **
+   ** speed: milliseconds until the next word show.
+   **/
+  $rotate.textrotator({
+    animation: 'dissolve', 
+    separator: '|', 
+    speed: 3500 
+  });
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() > window.innerHeight-100){ // position of menu from the top 
+      $nav.addClass('fixedClass');
+      if ($nav.data('size') == 'big') {
+        $nav.data('size','small').stop().animate({height: '60px'}, 300);
+      }
+    } else{
+      $nav.removeClass('fixedClass');
+      if ($nav.data('size') == 'small') {
+        $nav.data('size','big').stop().animate({height: '64px'}, 300);
+      }
+    }
+  });
 
 });
